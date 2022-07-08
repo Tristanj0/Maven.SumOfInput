a7a1bb462ffe16b35e490dee36a4ad618f90ce25
Tristan <tristan@Zipcoders-MacBook-Pro-2.local>
2022-07-08 09:23:54 -0400

    Found what to write from exam 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,10 @@
 public class Main {
 
     public static void main(String[] args){
+        Integer sum = 0;
+        for (int i = 1; i < sum + 1; i++) {
+            sum += i;
+        }
 
     }
 }
